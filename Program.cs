aebe11076cf4b95acfba3552333496ce72411752
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-14 22:10:08 -0300

    resolvido

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string cebolinha;
+
+Console.WriteLine("--- Exercico Cebolinha ---");
+
+Console.WriteLine(@"Eu troco as letras, não  falo bem, mas meus amigos não troco por ninguém
+Escreva a frase que deseja converter:"
+);
+
+cebolinha = Console.ReadLine();
+
+cebolinha = cebolinha.Replace("r", "l");
+
+Console.WriteLine(cebolinha);
